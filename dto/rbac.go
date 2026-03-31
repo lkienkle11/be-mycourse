29dba22cbb70aba76437201c79fa43a9e2c6e9fa
kienlt <kienlt@gmail.com>
2026-03-31 11:31:27 +0700

    refactor: rbac dtos

--- dto/rbac.go
+++ dto/rbac.go
@@ -1,26 +1,25 @@
 package dto
 
 type CreatePermissionRequest struct {
-	Code        string `json:"code" binding:"required,min=1,max=128"`
-	Description string `json:"description" binding:"omitempty,max=512"`
+	Code        string  `json:"code" binding:"required,min=1,max=128"`
+	CodeCheck   *string `json:"code_check"`
+	Description string  `json:"description" binding:"omitempty,max=512"`
 }
 
 type UpdatePermissionRequest struct {
 	Code        *string `json:"code"`
+	CodeCheck   *string `json:"code_check"`
 	Description *string `json:"description"`
 }
 
 type CreateRoleRequest struct {
 	Name        string `json:"name" binding:"required"`
 	Description string `json:"description"`
-	ParentID    *uint  `json:"parent_id,omitempty"`
 }
 
 type UpdateRoleRequest struct {
-	Name         *string `json:"name"`
-	Description  *string `json:"description"`
-	ParentID     *uint   `json:"parent_id,omitempty"`
-	RemoveParent bool    `json:"remove_parent,omitempty"`
+	Name        *string `json:"name"`
+	Description *string `json:"description"`
 }
 
 type SetRolePermissionsRequest struct {
@@ -29,4 +28,9 @@ type SetRolePermissionsRequest struct {
 
 type AssignUserRoleRequest struct {
 	RoleID uint `json:"role_id" binding:"required"`
+}
+
+type AssignUserPermissionRequest struct {
+	PermissionID *uint  `json:"permission_id"`
+	Code         string `json:"permission_code"`
 }

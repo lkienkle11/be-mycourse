445c7477f6b919268d4a3139bb5bc32017dc216d
kienlt <kienlt@gmail.com>
2026-04-15 14:48:43 +0700

    chore: fold rbac seed migration into base schema

--- migrations/000002_rbac_seed.down.sql
+++
@@ -1,7 +0,0 @@
--- Removes seeded RBAC rows. Also clears user role assignments (user_roles / user_permissions).
-
-DELETE FROM user_roles;
-DELETE FROM user_permissions;
-DELETE FROM role_permissions;
-DELETE FROM roles;
-DELETE FROM permissions;
